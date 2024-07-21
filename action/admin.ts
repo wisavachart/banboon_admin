98e44e288b5fbbac5c17e7cf57549cba2493c593
WIsavachart <wisavachart.s@gmail.com>
2024-07-21 09:41:48 +0700

    add-login-session

--- action/admin.ts
+++ action/admin.ts
@@ -1,29 +1,25 @@
 "use server";
+import { signIn } from "@/auth";
+import { CredentialsSignin } from "next-auth";
 import { redirect } from "next/navigation";
 const adminLogin = async (formData: FormData) => {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
   try {
-    if (email !== process.env.USERNAME && password !== process.env.PASSWORD) {
-      console.log("Wrong Credentials");
-      return;
+    await signIn("credentials", {
+      redirect: false,
+      callbackUrl: "/",
+      email,
+      password,
+    });
+  } catch (error) {
+    const someError = error as CredentialsSignin;
+    console.log("================ Login ไม่ได้");
+    if (someError) {
+      redirect("/login");
     }
-  } catch (err) {
-    console.log(err);
   }
-
-  // try {
-  //   await signIn("credentials", {
-  //     redirect: false,
-  //     callbackUrl: "/",
-  //     email,
-  //     password,
-  //   });
-  // } catch (error) {
-  //   const someError = error as CredentialsSignin;
-  //   return someError.cause;
-  // }
   redirect("/dashboard");
 };
 
